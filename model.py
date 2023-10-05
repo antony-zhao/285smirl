7877613b15a78bb9eb5fee5607f89de38cd81e10
antony-zhao <tonyzhao.davis@gmail.com>
2023-10-05 04:30:08 -0700

    test

---
+++ model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+from torch import nn
+import gymnasium as gym
+from gymnasium import spaces
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        pass
+
+    def forward(self, obs):
+        pass

5d33ad897956c6a4d31bf7eddbdf5581b3f77a72
antony-zhao <tonyzhao.davis@gmail.com>
2023-12-11 14:46:22 -0800

    pong changes

--- pong_smirl.py
+++ pong_smirl.py
@@ -13,9 +13,9 @@ from buffer import BernoulliBuffer, ReplayBuffer, GaussianBuffer
 from pettingzoo.utils import aec_to_parallel
 import supersuit as ss
 
-pong_train = pong_v3.env(num_players=2, obs_type='grayscale_image', full_action_space=False, max_cycles=1000)
+pong_train = pong_v3.env(num_players=2, obs_type='grayscale_image', full_action_space=False, max_cycles=2000)
 pong_eval = pong_v3.env(num_players=2, obs_type='grayscale_image', full_action_space=False, render_mode='human',
-                        max_cycles=1000)
+                        max_cycles=2000)
 
 
 def pong_wrappers(pong):
@@ -38,18 +38,18 @@ eval_env = SMIRLWrapper(pong_wrappers(pong_eval), GaussianBuffer,
 obs_space = env.observation_space(env.possible_agents[0])
 num_actions = env.action_space(env.possible_agents[0])
 agent_1 = DQNAgent(obs_space, num_actions, lr=1e-4, update_freq=1, start_after=10000,
-                   batch_size=256, target_update_freq=20000, eps_decay_per=2000, buffer=ReplayBuffer,
-                   filters=[[16, 5, 2], [32, 3, 2], [64, 2, 2]], normalize_rewards=False, capacity=int(1e5))
+                   batch_size=256, target_update_freq=20000, eps_decay_per=1000, buffer=ReplayBuffer,
+                   filters=[[16, 5, 2], [32, 3, 2], [64, 2, 2]], normalize_rewards=False, capacity=int(5e4))
 agent_2 = DQNAgent(obs_space, num_actions, lr=1e-4, update_freq=1, start_after=10000,
-                   batch_size=256, target_update_freq=20000, eps_decay_per=2000, buffer=ReplayBuffer,
-                   filters=[[16, 5, 2], [32, 3, 2], [64, 2, 2]], normalize_rewards=False, capacity=int(1e5))
+                   batch_size=256, target_update_freq=20000, eps_decay_per=1000, buffer=ReplayBuffer,
+                   filters=[[16, 5, 2], [32, 3, 2], [64, 2, 2]], normalize_rewards=False, capacity=int(5e4))
 
 rewards = {agent: [] for agent in env.possible_agents}
 losses = {agent: [] for agent in env.possible_agents}
 train_rewards = {agent: [] for agent in env.possible_agents}
 reward_temp = {agent: [] for agent in env.possible_agents}
 timestep = 0
-for ep in range(1000):
+for ep in range(5000):
     info = generate_trajectory_pz(env, [agent_1, agent_2])
     timestep += info["timestep"]
     for agent in env.possible_agents:
@@ -67,10 +67,13 @@ for ep in range(1000):
 print(timestep)
 for agent in env.possible_agents:
     plt.plot(rewards[agent])
+    np.savetxt(f'pong_reward_{agent}.csv', np.array(rewards[agent]))
     plt.show()
 
     plt.plot(losses[agent])
+    np.savetxt(f'pong_losses_{agent}.csv', np.array(rewards[agent]))
     plt.show()
 
     plt.plot(train_rewards[agent])
+    np.savetxt(f'pong_train_{agent}.csv', np.array(rewards[agent]))
     plt.show()
